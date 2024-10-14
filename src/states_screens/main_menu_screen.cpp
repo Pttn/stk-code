7b839e75139e6a825008419dfbb44d28429ef798
Pttn <28868425+Pttn@users.noreply.github.com>
2024-10-14 21:27:24 +0200

    Forbid Online Play.
    Signed-off-by: Pttn <28868425+Pttn@users.noreply.github.com>

--- src/states_screens/main_menu_screen.cpp
+++ src/states_screens/main_menu_screen.cpp
@@ -557,14 +557,8 @@ void MainMenuScreen::eventCallback(Widget* widget, const std::string& name,
     }
     else if (selection == "online")
     {
-        if(UserConfigParams::m_internet_status!=RequestManager::IPERM_ALLOWED)
-        {
-            new MessageDialog(_("You can not play online without internet access. "
-                                "If you want to play online, go in the options menu, "
-                                "and check \"Connect to the Internet\"."));
-            return;
-        }
-        OnlineScreen::getInstance()->push();
+        new MessageDialog(_("Their year is still 20XX..."));
+        return;
     }
     else if (selection == "addons")
     {
